8df59bd70fd473530e21814557a7ed25a97950c7
anchal00 <anchal82199@gmail.com>
2024-11-09 00:21:29 +0530

    Write basic test

---
+++ server/server_test.go
@@ -0,0 +1,82 @@
+package server
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.TestMode)
+	gs := setup()
+	if gs == nil {
+		os.Exit(1)
+	}
+	go gs.Run()
+	exitCode := m.Run()
+	tearDown(gs)
+	os.Exit(exitCode)
+}
+
+func setup() *GameServer {
+	err := godotenv.Load("../test.local")
+	if err != nil {
+		log.Print("Failed to load environment variables")
+		return nil
+	}
+	port := os.Getenv("DOODLE_PORT")
+	gs, err := NewGameServer(port)
+	if err != nil {
+		log.Printf("Failed to setup GameServer on port %s", port)
+		return nil
+	}
+	return gs
+}
+
+func tearDown(gs *GameServer) {
+	gs.Shutdown()
+	// remove sqlite file
+	err := os.Remove(fmt.Sprintf("%s.db", os.Getenv("DOODLE_DB")))
+	if err != nil {
+		log.Print("Failed to remove db file")
+	}
+}
+
+func ApiCall(method, path string, requestBody io.Reader) (*http.Response, error) {
+	url := fmt.Sprintf("http://localhost:%s%s%s", os.Getenv("DOODLE_PORT"), HTTP_API_V1_PREFIX, path)
+	req, err := http.NewRequest(method, url, requestBody)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Sending %s request to endpoint %s ", req.Method, req.URL)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("%s request to endpoint %s failed with error %s ", req.Method, req.URL, err.Error())
+		return nil, err
+	}
+	log.Printf("%s request to endpoint %s returned status %d", req.Method, req.URL, resp.StatusCode)
+	return resp, nil
+}
+
+func TestGamePlay(t *testing.T) {
+	// Create new game
+	createGameRequestBody, err := json.Marshal(map[string]any{
+		"player":       "rookie",
+		"max_players":  6,
+		"total_rounds": 4,
+	})
+	assert.Nil(t, err, "Failed to Create request body")
+	resp, err := ApiCall("POST", "/game", bytes.NewBuffer(createGameRequestBody))
+	assert.Nil(t, err, "Failed to execute CreateGame api call")
+	assert.Equal(t, http.StatusCreated, resp.StatusCode, "Failed to execute CreateGame api call")
+	// Add players to game
+}
